b4cdf317869c79f0c6e0260c86d906112979c1ac
kml92 <kevinloveee@gmail.com>
2022-05-24 18:54:24 -0700

    fix repo name set

--- index.php
+++ index.php
@@ -20,6 +20,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 require_once 'plugin-updater.php';
 $updater = new PDUpdater(__FILE__);
 $updater->set_username('kml92');
-$updater->set_repository('wordpress-plugin');
+$updater->set_repository('iframe-plugin');
 // $updater->authorize('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');
 $updater->initialize();
