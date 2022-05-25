57106bb265aa461ec2d8081da5468fb9ba739b22
kml92 <kevinloveee@gmail.com>
2022-05-25 08:29:51 -0700

    Remove auth token

--- iframe-plugin.php
+++ iframe-plugin.php
@@ -2,16 +2,13 @@
 /*
 	Plugin Name: Iframe Plugin
 	Description: This is for updating your Wordpress plugin.
-	Version: 1.0.1
+	Version: 1.0.2
 	Author: FYCLabs
 	Author URI: 
 */
-
-
 
 require_once 'plugin-updater.php';
 $updater = new PDUpdater(__FILE__);
 $updater->set_username('kml92');
 $updater->set_repository('iframe-plugin');
-// $updater->authorize('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');
 $updater->initialize();
